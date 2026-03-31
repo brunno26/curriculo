19cfcec5a973a00e53b11c98573371393ebe79bf
Bruno Gomes Laurentino <bruno.laurentino.adm@gmail.com>
2026-03-30 22:00:46 -0300

    Projeto CSS

--- js/curriculo.js
+++ js/curriculo.js
@@ -1,9 +1,5 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const botao = document.getElementById("toggle");
+const botao = document.getElementById("toggle");
 
-  if (botao) {
-    botao.addEventListener("click", function () {
-      document.body.classList.toggle("dark");
-    });
-  }
+botao.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
 });
